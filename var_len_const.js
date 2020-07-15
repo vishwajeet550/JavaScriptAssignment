e57d371a74006103f097de2db3c14514b9c8ab1e
vishwajeet550 <66294989+vishwajeet550@users.noreply.github.com>
2020-07-15 18:39:40 +0530

    Add files via upload

---
+++ var_len_const.js
@@ -0,0 +1,23 @@
+//var
+var name = "Amay";
+var age = 25;
+var city = "Pune";
+
+console.log(age)
+console.log(name)
+console.log("var is used for variable declaration. var is used to assign variable as string,number,boolean.")
+
+//let
+{
+    let name = "Raj";
+    let age = 35
+    console.log("Using Let:",name)
+    console.log("Using Let:",age)
+}
+console.log("let is used for variable declaration, but we declare variable name same many times thats why we use let. let is used to assign variable without disturbuining the another variable.")
+
+//Constant
+const country = "India";
+console.log(country);
+country = "UK"
+console.log("const is used for constant variable value hence we can't change the value.")
